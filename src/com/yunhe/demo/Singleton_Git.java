f816c7fccdfdb948840982b21843f4ef50ab9c50
lk <lk@qq.com>
2022-03-18 20:10:19 +0800

    修改错误代码

--- src/com/yunhe/demo/Singleton_Git.java
+++ src/com/yunhe/demo/Singleton_Git.java
@@ -6,6 +6,6 @@ package com.yunhe.demo;
 public class Singleton_Git {
     public static void main(String[] args) {
         System.out.println("测试");
-        System.out.println("pull")
+        System.out.println("pull");
     }
 }
