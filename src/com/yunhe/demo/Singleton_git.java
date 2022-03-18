f46e971e7e962b777aeee27447dc13b861995bca
lk <lk@qq.com>
2022-03-18 14:45:51 +0800

    修改一行代码

--- src/com/yunhe/demo/Singleton_git.java
+++ src/com/yunhe/demo/Singleton_git.java
@@ -5,6 +5,7 @@ package com.yunhe.demo;
  */
 public class Singleton_git {
     public static void main(String[] args) {
-        System.out.println("hello,world");
+       //测试
+        System.out.println("hello");
     }
 }
