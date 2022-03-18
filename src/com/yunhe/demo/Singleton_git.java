938836466fa9bf460629586c8cb6a57ea675198b
lk <lk@qq.com>
2022-03-18 14:43:53 +0800

    添加一个文件

---
+++ src/com/yunhe/demo/Singleton_git.java
@@ -0,0 +1,10 @@
+package com.yunhe.demo;
+
+/**
+ * @author ：lk
+ */
+public class Singleton_git {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
